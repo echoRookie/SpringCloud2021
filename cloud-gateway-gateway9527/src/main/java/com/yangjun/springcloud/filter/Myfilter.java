b5c07338a2b3404217b075511b33c74bdcb050bc
yangjun6 <yangjun6@meicloud.com>
2021-03-16 17:56:21 +0800

    feat:集成gateway网关添加全局过滤器

---
+++ cloud-gateway-gateway9527/src/main/java/com/yangjun/springcloud/filter/Myfilter.java
@@ -0,0 +1,33 @@
+package com.yangjun.springcloud.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author yangjun6
+ * @date 2021-03-16 17:25
+ */
+@Component
+public class Myfilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        String name = exchange.getRequest().getQueryParams().getFirst("name");
+        if(name == null){
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return  exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
