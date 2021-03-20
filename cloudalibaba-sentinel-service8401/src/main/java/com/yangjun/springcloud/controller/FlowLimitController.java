d33005c4241486a259a3193f4aafb9f7a6a1ed8a
echoRookie <echorookie@gmail.com>
2021-03-20 16:25:48 +0800

    feat:添加sentinel热点限流测试

--- cloudalibaba-sentinel-service8401/src/main/java/com/yangjun/springcloud/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/yangjun/springcloud/controller/FlowLimitController.java
@@ -1,6 +1,9 @@
 package com.yangjun.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,5 +19,14 @@ public class FlowLimitController {
     @GetMapping("/get/testB")
     public String testB(){
         return "Testb";
+    }
+    @GetMapping("/testhotkey")
+    @SentinelResource(value = "testhotkey", blockHandler = "dealHotkey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,@RequestParam(value = "p2", required = false) String p2){
+        return "热点限流测试";
+    }
+
+    public String dealHotkey(String p1, String p2, BlockException exception){
+        return "热点限流处理类";
     }
 }
